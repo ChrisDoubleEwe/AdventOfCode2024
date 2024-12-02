b0062f16da9c1686b3ff5a8616b40efc337b7bec
Chris Walsh <chris@offwhitehouse.co.uk>
2024-12-02 06:33:51 +0000

    Day 1

--- 01.py
+++ 01.py
@@ -1,11 +1,36 @@
-parta = 0;
 filename = '01_in.txt';
+left = [];
+right = [];
 
 with open(filename) as f:
   for l in f:
     line = l.strip()
+    left.append(int(line.split('   ')[0]));
+    right.append(int(line.split('   ')[1]));
 
-    print line;
+left.sort();
+right.sort();
 
-print("PART A: " + str(parta));
+diff = 0;
+for i in range(0, len(left)):
+   diff += abs(left[i] - right[i])
+
+print("PART A: " + str(diff));
+
+
+
+partb = 0
+for i in range(0, len(left)):
+  num = left[i];
+  count = 0;
+  for j in range(0, len(left)):
+    if right[j] == num:
+      count += 1;
+  partb += num * count;
+
+print("PART B: " + str(partb));
+
+
+
+
 
